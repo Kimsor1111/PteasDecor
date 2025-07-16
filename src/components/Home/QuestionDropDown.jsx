fd7b24e8c01d9634fcc53c641b27882e082a5b0a
Danin <ninsweetie859@gmail.com>
2025-07-16 15:41:39 +0700

    modify question dropdown

--- src/components/Home/QuestionDropDown.jsx
+++ src/components/Home/QuestionDropDown.jsx
@@ -5,7 +5,9 @@ const QuestionDropDown = ({ question, answer }) => {
   const [dropdown, setDropdown] = useState(false);
   return (
     <div
-      className="w-full border-[1px] rounded border-[#A6A6A6] cursor-pointer p-4 selection:bg-transparent"
+      className={`w-full ${
+        dropdown ? "h-[140px]" : "h-[55px]"
+      } border-[1px] rounded border-[#A6A6A6] transition-[height] duration-400 cursor-pointer p-4 selection:bg-transparent`}
       onClick={() => setDropdown(!dropdown)}
     >
       <h1 className="w-full relative text-[#222222] lg:text-sm text-sm pr-5">
@@ -15,9 +17,9 @@ const QuestionDropDown = ({ question, answer }) => {
           className="absolute right-0 top-1/2 -translate-y-1/2"
         />
       </h1>
-      {dropdown && (
-        <p className="text-[#666666] lg:text-sm text-[12px] mt-5">{answer}</p>
-      )}
+      {/* {dropdown && ( */}
+      <p className={`${dropdown? 'opacity-100 translate-y-0':'opacity-0 translate-y-[-30px]'} transition-all duration-300 text-[#666666] lg:text-sm text-[12px] mt-5`}>{answer}</p>
+      {/* )} */}
     </div>
   );
 };
